dbbb3e23495cfaeed5a2e0a3e556188affd772f8
mattymatty <matteo.francesconi@davide.it>
2018-03-22 18:04:44 +0100

    improved embedded help spelling

--- src/main/java/com/testBot/MyListener.java
+++ src/main/java/com/testBot/MyListener.java
@@ -8,49 +8,48 @@ import net.dv8tion.jda.core.hooks.ListenerAdapter;
 
 import java.awt.*;
 import java.sql.*;
-
-import java.util.Arrays;
 import java.util.List;
 
 public class MyListener extends ListenerAdapter {
     private Connection conn;
     private List<BotGuild> savedGuilds;
 
-    private static String toSendUser =
-            "help of testbot\n" +
-                    "- ping: answers Pong!";
-    private static String toAddMod = "\n\n" +
-            "mod commands:\n" +
-            "- prefix [prefix]: changes bot prefix for this server\n" +
-            "- modrole <add/remove/list> [roleMention]: add a role to modroles\n" +
-            "- role <add/remove> [rolemention] : add yourself a role";
-
     @Override
     public void onMessageReceived(MessageReceivedEvent event) {
+        //if is a direct message exit immediately
+        if(event.isFromType(ChannelType.PRIVATE)) return;
+        //if is a bot exit immediately
+        if (event.getAuthor().isBot()) return;
         BotGuild guild;
+        //name of sender server
         String guildname = event.getGuild().getName();
+        //search for existent informations class for server
         guild = findGuild(event.getGuild().getIdLong());
         if (guild == null) {
+            //create local instance of server informations
             guild = new BotGuild(event.getGuild().getIdLong(), guildname.intern(), conn);
             savedGuilds.add(guild);
         }
-        if (event.getAuthor().isBot()) return;
+        //get sender member
         Member member = event.getMember();
+        //get channel to send
         MessageChannel channel = event.getChannel();
+        //get message
         Message message = event.getMessage();
+        //get text
         String content = message.getContent();
         System.out.println("Message from '" + member.getEffectiveName() + "' in guild '" + guildname + "'");
+        //if length is enough test if the message starts with right prefix
         if (content.length() > guild.getPrefix().length() && content.substring(0, guild.getPrefix().length()).equals(guild.getPrefix())) {
+            //split by spaces into args
             String[] args = content.substring(guild.getPrefix().length()).split(" ");
+            //test first argument
             switch (args[0]) {
 
 //------USER---------------------HELP--------------------------------------
 
                 case "help":
                     System.out.println("help shown in guild: '" + guildname + "'");
-                    /*channel.sendMessage(toSendUser).queue();
-                    if (member.isOwner() || guild.memberIsMod(member))
-                        channel.sendMessage(toAddMod).queue();*/
                     PrintHelp(channel, member, guild);
                     break;
 
@@ -65,8 +64,11 @@ public class MyListener extends ListenerAdapter {
 //------MOD---------------------SET----------------------------------------
 
                 case "prefix":
+                    //if member is allowed
                     if (member.isOwner() || guild.memberIsMod(member)) {
+                        //if there is a second arg
                         if (args[1] != null) {
+                            //if is not too long
                             if (args[1].length() > 10) {
                                 System.out.println("prefix set failed in guild: '" + guildname + "'");
                                 channel.sendMessage("Error too long prefix (limit is 10)!").queue();
@@ -88,12 +90,18 @@ public class MyListener extends ListenerAdapter {
 //-------MOD-------------------MODROLE-------------------------------------
 
                 case "modrole":
+                    //if member is allowed
                     if (member.isOwner() || guild.memberIsMod(member)) {
+                        //if there are other arguments
                         if (args[1] != null) {
+                            //get mentioned roles
                             List<Role> mentions = message.getMentionedRoles();
+                            //test on second arg
                             switch (args[1]) {
                                 case "add":
+                                    //if there is a mentioned role
                                     if (mentions.size() == 1) {
+                                        //call class method to add roles
                                         System.out.println("adding modrole '" + mentions.get(0).getName() + "' to guild '" + guildname + "'");
                                         guild.addModRole(mentions.get(0).getIdLong(), mentions.get(0).getName());
                                         channel.sendMessage("Role added!").queue();
@@ -103,7 +111,9 @@ public class MyListener extends ListenerAdapter {
                                     }
                                     break;
                                 case "remove":
+                                    //if there is a mentioned user
                                     if (mentions.size() == 1) {
+                                        //call class method to remove roles
                                         System.out.println("removing modrole '" + mentions.get(0).getName() + "' from guild '" + guildname + "'");
                                         if (guild.removeModRole(mentions.get(0).getIdLong()) != null)
                                             channel.sendMessage("Role removed!").queue();
@@ -115,11 +125,13 @@ public class MyListener extends ListenerAdapter {
                                     }
                                     break;
                                 case "list":
+                                    //list all modroles
                                     System.out.println("listing modroles in guild: '" + guildname + "'");
                                     StringBuilder text = new StringBuilder("Active ModRoles:\n");
                                     for (Long id : guild.getModRolesById()) {
                                         for (Role role : event.getGuild().getRoles()) {
                                             if (role.getIdLong() == (id))
+                                                //gets the role name whit api method
                                                 text.append(role.getName()).append("\n");
                                         }
                                     }
@@ -142,14 +154,20 @@ public class MyListener extends ListenerAdapter {
 //-------MOD-----------------------ROLE------------------------------------------------
 
                 case "role":
+                    //if the member is allowed
                     if (member.isOwner() || guild.memberIsMod(member)) {
+                        //if there are other args
                         if (args[1] != null) {
+                            //get mentions list
                             List<Role> mentions = message.getMentionedRoles();
                             switch (args[1]) {
                                 case "add":
+                                    //if there is a mention
                                     if (mentions.size() == 1) {
+                                        //test if your higher role is higher than the one you're setting
                                         List<Role> roles = event.getGuild().getSelfMember().getRoles();
                                         if (roles.get(0).getPosition() > mentions.get(0).getPosition()) {
+                                            //add role using api method
                                             event.getGuild().getController().addRolesToMember(member, mentions).queue();
                                             channel.sendMessage("Role added!").queue();
                                             System.out.println("added a role to '" + member.getEffectiveName() + "'in guild: '" + guildname + "'");
@@ -163,7 +181,9 @@ public class MyListener extends ListenerAdapter {
                                     }
                                     break;
                                 case "remove":
+                                    //if there is a mention
                                     if (mentions.size() == 1) {
+                                        //test if your higher role is higher than the one you're setting
                                         List<Role> roles = event.getGuild().getSelfMember().getRoles();
                                         if (roles.get(0).getPosition() > mentions.get(0).getPosition()) {
                                             event.getGuild().getController().removeRolesFromMember(member, mentions).queue();
@@ -240,10 +260,12 @@ public class MyListener extends ListenerAdapter {
 //-------ALL---------------------------IGNORED--------------------------------
 
         } else {
+            //if the message was not directed to the bot
             System.out.println("Ignored");
         }
     }
 
+    //need explanation?
     private BotGuild findGuild(Long guildId) {
         for (BotGuild guild : savedGuilds) {
             if (guild.getId().equals(guildId))
@@ -252,6 +274,7 @@ public class MyListener extends ListenerAdapter {
         return null;
     }
 
+    //prints the help message
     private void PrintHelp(MessageChannel channel, Member member, BotGuild guild) {
         EmbedBuilder message = new EmbedBuilder();
 
@@ -261,6 +284,7 @@ public class MyListener extends ListenerAdapter {
         message.addField("help", "shows this help", false);
         message.addField("ping", "answers pong (userfull for speed tests and alive check)", false);
 
+        //if 
         if (member.isOwner() || guild.memberIsMod(member)) {
             message.addBlankField(false);
             message.addField("MOD commands:", "", false);
