a5f0bdeb35a1d4e391c8f56d7dfdd6d0373c4c5c
mattymatty <matteo.francesconi@davide.it>
2018-03-22 18:14:38 +0100

    changed database auth

--- src/main/java/com/testBot/BOT.java
+++ src/main/java/com/testBot/BOT.java
@@ -4,6 +4,7 @@ import net.dv8tion.jda.core.AccountType;
 import net.dv8tion.jda.core.JDA;
 import net.dv8tion.jda.core.JDABuilder;
 import java.sql.*;
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
@@ -32,19 +33,19 @@ public class BOT
             return;
         }
         try {
-            url = "jdbc:postgresql://ec2-54-247-81-88.eu-west-1.compute.amazonaws.com/d5a36f2opkuv4a";
-            Properties props = new Properties();
-            props.setProperty("user","hmefwffiqybapa");
-            props.setProperty("password","c054e4e3cbca7e7695c3b87d104e87777ee952fde4891516236f074e0e4fb7ec");
-            props.setProperty("ssl","true");
-            props.setProperty("sslfactory","org.postgresql.ssl.NonValidatingFactory");
-            conn = DriverManager.getConnection(url, props);
-            //conn = DriverManager.getConnection("jdbc:mysql://84.201.37.95/Bot?user=Bot&password=BotPassword");
+            URI dbUri = new URI(System.getenv("DATABASE_URL"));
+
+            String username = dbUri.getUserInfo().split(":")[0];
+            String password = dbUri.getUserInfo().split(":")[1];
+            String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+
+            conn = DriverManager.getConnection(dbUrl, username, password);
             System.out.println("SQL INITIALIZZATED");
         } catch (SQLException ex) {
             System.out.println("SQLException: " + ex.getMessage());
             System.out.println("SQLState: " + ex.getSQLState());
             System.out.println("VendorError: " + ex.getErrorCode());
+            System.exit(-1);
         }
 
         JDA api = new JDABuilder(AccountType.BOT).setToken("NDIwNTY0MTU1NjQ5NjIyMDE4.DYA5dA._MmdVLt7jHqwlJpbEI4YE07ULxs").buildAsync();
